f4c7eba6f780b78b30b10f4e5690488707577215
Keith Holliday <krh121791@gmail.com>
2016-12-11 12:45:07 -0600

    Removed extra config

--- twitchtube/twitch/twitchConfig-example.py
+++
@@ -1,5 +0,0 @@
-HOST = "irc.chat.twitch.tv"
-NICK = "Replace with Nickname of Bot"
-PORT = 6667
-#Get an OAuth password from https://www.twitch.tv/settings/connections. Register at the bottom
-PASS = "Replace with OAuth password"
