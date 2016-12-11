b028bb05fac2e68257038c2caf1b113df77b2a53
Keith Holliday <krh121791@gmail.com>
2016-12-11 12:46:07 -0600

    Reorganized model tests

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import unittest
 
 from tests.test_example import PrimesTestCase
-from tests.twitch.test_YoutubeMessageModel import YoutubeMessageModelTestCase
-from tests.twitch.test_TwitchMessageFromYouTube import YouTubeMessageModelTestCase
+
+from tests.models.test_YoutubeMessageModel import YoutubeMessageModelTestCase
+from tests.models.test_TwitchMessageFromYouTube import YouTubeMessageModelTestCase
 
 if __name__ == '__main__':
     unittest.main()
