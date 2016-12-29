7aa16aadf59a2c5f2bab4fdda90b11a7e4a5740a
Keith Holliday <krh121791@gmail.com>
2016-12-28 19:54:43 -0600

    Fixed bot command. And file name

--- scripts/recieveBotCommands.py
+++ scripts/recieveBotCommands.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import pika
 import json
 import os
 import signal
+from time import sleep
 
 # abspath = os.path.abspath(__file__)
 # dname = os.path.dirname(abspath)
@@ -12,82 +12,25 @@ workingDirectory = os.getcwd();
 import config
 queueName = config.rabbitQueue
 
-def getPidForBot(botId):
-    pid = None
-    filename = "tmp/" + botId + ".txt"
-    try:
-        with open(filename) as pidFile:
-            pid = pidFile.readlines()
-            pid = int(pid[0])
-    except:
-        print "No process"
+import redis
+r = redis.StrictRedis()
+reddisBotCommandQueue = config.reddisBotCommandQueue
 
-    return pid
+def applyActionToBot(botId, action):
+    if action == 'start':
+        startScript = "pm2 {Action} --name={BotId} init.py -- --botId={BotId}"
+    else:
+        startScript = "pm2 {Action} {BotId}"
+    botStartScript = startScript.replace("{Action}", str(action)).replace("{BotId}", str(botId))
+    os.system(botStartScript)
 
-def stopBot(pid, filename, botId):
-    try:
-        os.kill(pid, signal.SIGTERM)
-    except:
-        print("Error")
+while 1:
+    queue = r.lpop(reddisBotCommandQueue)
 
-    try:
-        os.remove(filename)
-    except:
-        print("Error")
-    print ("stoping %s" % botId)
+    if queue is not None:
+        command = json.loads(queue.decode())
+        botId = command['botId']
+        action = command['status']
+        applyActionToBot(botId, action)
 
-def applyActionToBot(botId, action, pid):
-    startScript = "python init.py --botId={BotId} > tmp/logs-{BotId}.txt 2>&1 &"
-    filename = "tmp/" + botId + ".txt"
-
-    botStartScript = startScript.replace("{BotId}", str(botId))
-
-    if action == "start":
-        if pid is not None:
-            print "Cannot start"
-            return
-
-        os.system(botStartScript)
-        print ("starting %s" % botId)
-
-    elif action == "restart":
-        if pid is None:
-            print "Cannot restart"
-            return
-
-        stopBot(pid, filename, botId)
-        os.system(botStartScript)
-
-        print ("restarted %s" % botId)
-
-    elif action == "stop":
-        if pid is None:
-            print "Cannot stop"
-            return
-
-        stopBot(pid, filename, botId)
-
-        print ("stoping %s" % botId)
-
-
-def callback(ch, method, properties, body):
-    command = json.loads(body)
-    botId = command['botId']
-    action = command['status']
-
-    pid = getPidForBot(botId)
-
-    applyActionToBot(botId, action, pid)
-
-    print(" [x] Received %r" % command)
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
-channel.queue_declare(queue=queueName)
-channel.basic_consume(callback,
-                      queue=queueName,
-                      no_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    sleep(.5)
