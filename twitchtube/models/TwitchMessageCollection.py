d9395716498a8ec8f5f50554e6c08ed8e2dfcaaf
Keith Holliday <krh121791@gmail.com>
2017-04-09 17:54:40 -0600

    Fixed linting on TwitchMessageCollection

--- twitchtube/models/TwitchMessageCollection.py
+++ twitchtube/models/TwitchMessageCollection.py
@@ -1,44 +1,14 @@
-import datetime
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-
-# @TODO: Move somwehre else?
-# import config
-# client = MongoClient(config.mongoUrl)
-# db = client[config.database]
-# mongoChat = db.twitchMessages
-
+'''Manages the colleciton of message on the
+    Redis TwitchMessageToSync list'''
 import redis
-# r = redis.StrictRedis()
-
 import config
-r = redis.from_url(config.redisURL)
 
 class TwitchMessageCollection(object):
-    def __init__(self, bot):
-        self.botId = bot['_id']
+    '''Manages the colleciton of message on the
+    Redis TwitchMessageToSync list'''
+    def __init__(self):
+        self.redis = redis.from_url(config.redisURL)
 
-    def getNextMessageToSend(self):
-        return r.lpop("twtichMessageToSync" + str(self.botId))
-        self.mongoDocument = mongoChat.find_one({
-            "sent": False, "bot_id": ObjectId(self.botId),
-            "date": {
-                "$gt": datetime.datetime.now() - datetime.timedelta(minutes=3)
-            },
-        })
-        return self.mongoDocument
-
-    def markSent(self):
-        result = mongoChat.update_one(
-            {"_id": self.mongoDocument['_id']},
-            {
-                "$set": {
-                    "sent": True
-                },
-                "$currentDate": {"lastModified": True}
-            }
-        )
-
-    # @TODO: Can we enforce type of the model?
-    def saveChat(self, messageMongoObject):
-        mongoChat.insert_one(messageMongoObject)
+    def get_next_message_to_send(self):
+        '''Gets next message on the redis stack'''
+        return self.redis.lpop("TwitchMessageToSync")
